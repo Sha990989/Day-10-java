14aa47981affa148e0bbd1978962eacaf2beabab
Sha990989 <anavaneethasankar@gmail.com>
2024-12-11 22:23:26 +0530

    Add files via upload

---
+++ parameterizedconstructor.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class a{
+    int c;
+    String b;
+    a(int c,String b)
+    {
+        this.c=c;
+        this.b=b;
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+}
+public class parameterizedconstructor
+{
+    public static void main(String args[])
+    {
+        a s=new a(1517,"sana");
+        s.display();
+    }
+}
