c85f1c1b5c674b8ba95934933b87d0e973300755
Sha990989 <anavaneethasankar@gmail.com>
2024-12-11 13:27:09 +0530

    Add files via upload

---
+++ staticwithnonvoidmethod.java
@@ -0,0 +1,13 @@
+public class staticwithnonvoidmethod {
+    public static String s()
+    {
+        return "Hello World";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    
+    }
+    
+}
