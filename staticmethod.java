97ae17ec6cc549d6c245affeb06bce7c6c1cf02d
Sha990989 <anavaneethasankar@gmail.com>
2024-12-11 11:03:50 +0530

    Add files via upload

---
+++ staticmethod.java
@@ -0,0 +1,11 @@
+public class staticmethod {
+    public static void s()
+    {
+        System.out.println("Navaneetha");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+    
+}
