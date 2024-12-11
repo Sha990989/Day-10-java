28ed7107a2b709c217138e925b6f9edf2b61057b
Sha990989 <anavaneethasankar@gmail.com>
2024-12-11 13:27:36 +0530

    Add files via upload

---
+++ staticwithoutaccessmodifier.java
@@ -0,0 +1,13 @@
+//we can access without access modifier 
+public class staticwithoutaccessmodifier {
+    static String s()
+    {
+        return "Hello Students";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+    
+}
