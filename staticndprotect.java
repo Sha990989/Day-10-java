a1570a0df54edbb610851a49fc136c10bcf6834b
Sha990989 <anavaneethasankar@gmail.com>
2024-12-11 13:26:19 +0530

    Add files via upload

---
+++ staticndprotect.java
@@ -0,0 +1,12 @@
+//protected access modifier with static method
+public class staticndprotect {
+    protected static void s()
+    {
+     System.out.println("Shalini");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+    
+}
